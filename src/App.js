591d400de2a504b0b6fcd13ec1c6b6848130f35a
sadmanador <107940313+GaziTasnuvaFarhana@users.noreply.github.com>
2022-11-26 16:33:46 +0600

    module 56-7 done!, only module summary left

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import app from './firebase/firebase.init';
 import { useState } from 'react';
 
@@ -7,12 +7,13 @@ import { useState } from 'react';
 const auth = getAuth(app);
 
 function App() {
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const gitHubProvider = new GithubAuthProvider();
 
   const [user, setUser] = useState({})
 
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then(result => {
         const user = result.user;
         setUser(user)
@@ -21,6 +22,18 @@ function App() {
       .catch(error => {
         console.error('error: ', error)
       })
+  }
+
+  const handleGitHubSingIn = () => {
+    signInWithPopup(auth, gitHubProvider)
+    .then(result => {
+      const user = result.user;
+      setUser(user);
+      console.log(user);
+    })
+    .catch(error => {
+      console.error('error: ', error)
+    })
   }
 
   const handleSignOut = () => {
@@ -37,12 +50,16 @@ function App() {
   return (
     <div className="App">
       {
-        user.email ?
-        <button onClick={handleSignOut}>sign-out</button>
-        :
-        <button onClick={handleGoogleSignIn}>Google signin</button>
+        user.uid ?
+          <button onClick={handleSignOut}>sign-out</button>
+          :
+          <>
+            <button onClick={handleGoogleSignIn}>Google signin</button>
+            <button onClick={handleGitHubSingIn}>Signin with github</button>
+          </>
+
       }
-      {user.email && <div className="user-info">
+      {user.uid && <div className="user-info">
         <h2>User: {user.displayName}</h2>
         <p>email: {user.email}</p>
         <img src={user.photoURL} alt="" />
